136596b1cfb5eb4ad0d4505f51442e865252ae43
Pree <siktheday@gmail.com>
2023-12-04 10:38:08 -0500

    day 1 part 1

---
+++ 1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const data = fs.readFileSync("data", { encoding: "utf8", flag: "r" });
+
+let datArray = data.split("\n");
+
+//first number
+function sum() {
+  let total = 0;
+
+  for (let i = 0; i < datArray.length; i++) {
+    let digits = datArray[i].replace(/\D/g, "");
+    let number = digits[0] + digits[digits.length - 1];
+    total += +number;
+  }
+  return total;
+}
+// total is 54081
